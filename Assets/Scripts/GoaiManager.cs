85c3f885de6e6e7f66ac49f04fb395bcd7ccc373
KotaGit <Kota7arai@gmail.com>
2021-08-04 23:53:35 +0900

    当たり判定
    当たり判定

---
+++ Assets/Scripts/GoaiManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoaiManager : MonoBehaviour
+{
+
+    [Header("プレイヤー")]public GameObject player;
+    [Header("テキスト")]public GameObject text;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+       
+    }
+
+
+   
+
+    //当たり判定
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == player.name)
+        {
+            text.GetComponent<Text>().text = "Goal!!";
+            text.SetActive(true);    
+            Debug.Log("何かがせっしょくした");
+        }
+    }
+    
+}
