074669105da2a67953b1501c140d40de053c5ac6
KotaGit <Kota7arai@gmail.com>
2021-08-09 16:50:14 +0900

    落下時の処理実装
    落下時の処理実装

---
+++ Assets/Scripts/DropManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropManager : MonoBehaviour
+{
+    [Header("プレイヤー")]public GameObject player;
+    [Header("テキスト")] public GameObject text;
+
+    //プライベート変数
+    //RestartMananager型
+    private RestartManager restart;
+
+    void Start()
+    {
+        //インスタンス生成
+        restart = new RestartManager(player, text);
+    }
+
+    
+    void Update()
+    {
+        if(player.transform.position.y < -10)
+        {
+            restart.PrintGameOver();
+        }
+
+        if(restart.IsGameOver() && Input.GetMouseButton(0))
+        {
+            restart.Restart();
+        }
+    }
+}
