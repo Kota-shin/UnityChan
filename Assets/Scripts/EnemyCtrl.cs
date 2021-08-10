1b08e2db7cb6d5ce0504b4d8705c335b74663011
KotaGit <Kota7arai@gmail.com>
2021-08-10 22:01:45 +0900

    追尾機能、追いかける敵実装
    追尾機能、追いかける敵実装

---
+++ Assets/Scripts/EnemyCtrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyCtrl : MonoBehaviour
+{
+    //追跡するオブジェクトのトランスフォーム
+    public Transform targer;
+    //エージェントとなるオブジェクトのNavMeshAgent
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        //目的地となる座標を設定する
+        agent.destination = targer.position;
+    }
+}
---
+++ Assets/Scripts/EnemyHitCtrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitCtrl : MonoBehaviour
+{
+    [Header("プレイヤー")] public GameObject player;
+    [Header("テキスト")] public GameObject text;
+
+    //プライベート変数
+    //RestartManager型
+    private RestartManager restart;
+
+    void Start()
+    {
+        //インスタンス生成
+        restart = new RestartManager(player, text);
+    }
+
+
+    void Update()
+    {
+        if(restart.IsGameOver() && Input.GetMouseButton(0))
+        {
+            restart.Restart();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //接触したのがプレイヤーだった時
+        if(other.gameObject.name == player.name)
+        {
+            restart.PrintGameOver();
+        }
+    }
+}
